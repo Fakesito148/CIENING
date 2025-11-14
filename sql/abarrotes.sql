54cf8baeb8cda26b7427763b0877c28f17b5a57b
Fakesito148 <marcelo.mier.dm@gmail.com>
2025-11-14 06:10:21 -0600

    BD ACTUALIZADA

--- sql/abarrotes.sql
+++ sql/abarrotes.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2025 a las 05:33:45
+-- Tiempo de generación: 14-11-2025 a las 13:05:55
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,8 +41,8 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`, `correo`) VALUES
-(1, 'Pedro de Jesús', 'Cervantes Morales', 'PedroC0909', 'Coco0120', 'pedro.cervantesmls@uanl.edu.mx'),
-(2, 'Carlos ', 'Martinez Mendoza', 'Carlos2888', 'Dreamfyre88', 'Carlos_mm01@gmail.com');
+(1, 'Pedro de Jesús', 'Cervantes Morales', 'PedroC0909', 'Fime255', 'pedro.cervantesmls@uanl.edu.mx'),
+(2, 'Marcelo', 'Mier', 'MM13', '12345', 'marcelo.mier.dm@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -62,9 +62,11 @@ CREATE TABLE `dulces` (
 --
 
 INSERT INTO `dulces` (`id`, `nombre`, `cantidad`, `estado`) VALUES
-(1, 'Paleta', 200, 'Suficiente'),
-(2, 'Chocolate', 120, 'Suficiente'),
-(3, 'Chicle', 300, 'Suficiente');
+(5, 'Paleta Tutsi Pop', 54, 'Suficiente'),
+(6, 'Chocolate Oscuro Hersheys', 24, 'Suficiente'),
+(7, 'Panditas Clasicos', 15, 'Suficiente'),
+(8, 'Mazapan de la Rosa', 27, 'Suficiente'),
+(9, 'Chocolate Carlos V semiamargo', 50, 'Suficiente');
 
 -- --------------------------------------------------------
 
@@ -86,8 +88,8 @@ CREATE TABLE `empleados` (
 --
 
 INSERT INTO `empleados` (`id`, `nombre`, `apellido`, `usuario`, `contraseña`, `correo`) VALUES
-(1, 'Jorge', 'Hernández', 'Jorge0815', 'Night_city15', 'Jorge_hdz1998@gmail.com'),
-(2, 'Miguel Angel', 'Robles Hernán', 'Angel_RH2209', 'Black_cat0990', 'Miguel_RH09@outlook.com');
+(1, 'Nicolás', 'Hernández', 'Nico0815', 'Night_city15', 'nicolas_hdz1998@gmail.com'),
+(2, 'Marcelo', 'Mier', 'mm_e13', '12345', 'mmempleado@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -107,7 +109,7 @@ CREATE TABLE `galletas` (
 --
 
 INSERT INTO `galletas` (`id`, `nombre`, `cantidad`, `estado`) VALUES
-(1, 'Marías', 80, 'Suficiente'),
+(1, 'Marias', 80, 'Suficiente'),
 (2, 'Oreo', 60, 'Suficiente'),
 (3, 'Chokis', 90, 'Suficiente');
 
@@ -129,10 +131,12 @@ CREATE TABLE `lacteos` (
 --
 
 INSERT INTO `lacteos` (`id`, `nombre`, `cantidad`, `estado`) VALUES
-(1, 'Leche', 100, 'Suficiente'),
-(2, 'Queso', 50, 'Suficiente'),
-(3, 'Crema', 30, 'Suficiente'),
-(4, 'Yogur', 70, 'Suficiente');
+(6, 'Leche Lala 100 Light', 90, 'Suficiente'),
+(7, 'Mantequilla Lala 4 Pack', 19, 'Suficiente'),
+(8, 'Yoghurt lala zero', 40, 'Suficiente'),
+(9, 'Queso manchego LALA', 10, 'Suficiente'),
+(10, 'Crema LALA', 20, 'Suficiente'),
+(11, 'Nutrileche', 30, 'Suficiente');
 
 -- --------------------------------------------------------
 
@@ -179,7 +183,7 @@ CREATE TABLE `tickets_recuperacion` (
 
 INSERT INTO `tickets_recuperacion` (`id`, `nombre`, `apellido`, `tipo`, `usuario`, `correo`, `fecha`, `estado`) VALUES
 (1, 'Pedro de Jesús', 'Cervantes Morales', 'admins', 'PedroC0909', 'pedro.cervantesmls@uanl.edu.mx', '2025-09-06', 'PENDIENTE'),
-(2, 'Nicolás', 'Hernández', 'empleados', 'Nico0815', 'nicolas_hdz1998@gmail.com', '2025-09-06', 'COMPLETADO');
+(2, 'Nicolás', 'Hernández', 'empleados', 'Nico0815', 'nicolas_hdz1998@gmail.com', '2025-09-06', 'PENDIENTE');
 
 -- --------------------------------------------------------
 
@@ -202,6 +206,30 @@ INSERT INTO `variados` (`id`, `nombre`, `cantidad`, `estado`) VALUES
 (1, 'Refresco', 150, 'Suficiente'),
 (2, 'Agua', 200, 'Suficiente'),
 (3, 'Jugo', 90, 'Suficiente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `seccion` varchar(50) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`id`, `producto_id`, `seccion`, `cantidad`, `fecha`) VALUES
+(1, 6, 'lacteos', 3, '2025-11-12 21:39:35'),
+(2, 5, 'dulces', 1, '2025-11-12 21:41:35'),
+(3, 6, 'dulces', 6, '2025-11-12 21:41:37'),
+(4, 8, 'dulces', 3, '2025-11-12 21:41:39');
 
 --
 -- Índices para tablas volcadas
@@ -256,6 +284,12 @@ ALTER TABLE `variados`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -269,7 +303,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT de la tabla `dulces`
 --
 ALTER TABLE `dulces`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `empleados`
@@ -287,7 +321,7 @@ ALTER TABLE `galletas`
 -- AUTO_INCREMENT de la tabla `lacteos`
 --
 ALTER TABLE `lacteos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `papitas`
@@ -306,6 +340,12 @@ ALTER TABLE `tickets_recuperacion`
 --
 ALTER TABLE `variados`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
